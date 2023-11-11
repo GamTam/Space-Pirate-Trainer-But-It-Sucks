e8f21cf378f074e3c3042338def48b38309b17d0
Ellie <117389757+ellieInfinity@users.noreply.github.com>
2023-11-10 22:44:35 -0500

    Added basic UI

---
+++ Assets/Scripts/UI/ScoreBoardManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreBoardManager : MonoBehaviour
+{
+    [SerializeField] TextMeshPro scoreText;
+    [SerializeField] private int score;
+    [SerializeField] private int lives = 3;
+    [SerializeField] private int waves = 1;
+    
+    
+        // Update is called once per frame
+        void Update()
+        {
+            scoreText.text = "Score: " + score + "\n\nLives: " + lives + "/3\n\nWave: " + waves;
+        }
+
+        public void IncreaseScore100()
+        {
+            score += 100;
+        }
+        
+        public void IncreaseScore250()
+        {
+            score += 250;
+        }
+        
+        public void IncreaseScore2500()
+        {
+            score += 2500;
+        }
+
+        public void DecreaseLives()
+        {
+            lives--;
+        }
+        
+        public void IncreaseLives()
+        {
+            lives++;
+        }
+
+        public void IncreaseWave()
+        {
+            waves++;
+        }
+}
